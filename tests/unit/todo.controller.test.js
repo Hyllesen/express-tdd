64038cad60d3dbc736ae7ae69cf9000fa3ebb53a
stefan <stefan@stefans-iMac.local>
2019-08-19 12:36:11 +0200

    Created test for TodoModel.create

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,7 +1,14 @@
 const TodoController = require("../../controllers/todo.controller");
+const TodoModel = require("../../model/todo.model");
+
+TodoModel.create = jest.fn();
 
 describe("TodoController.createTodo", () => {
   it("should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
+  });
+  it("should call TodoModel.create", () => {
+    TodoController.createTodo();
+    expect(TodoModel.create).toBeCalled();
   });
 });
