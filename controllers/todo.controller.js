7cb8ce9a4ad7fab27ecadb55a7296559fe3b81e7
stefan <stefan@stefans-iMac.local>
2019-08-19 13:09:19 +0200

    Using request.body contents

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,5 +1,5 @@
 const TodoModel = require("../model/todo.model");
 
-exports.createTodo = () => {
-  TodoModel.create();
+exports.createTodo = (req, res, next) => {
+  TodoModel.create(req.body);
 };
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,5 +1,7 @@
 const TodoController = require("../../controllers/todo.controller");
 const TodoModel = require("../../model/todo.model");
+const httpMocks = require("node-mocks-http");
+const newTodo = require("../mock-data/new-todo.json");
 
 TodoModel.create = jest.fn();
 
@@ -8,7 +10,12 @@ describe("TodoController.createTodo", () => {
     expect(typeof TodoController.createTodo).toBe("function");
   });
   it("should call TodoModel.create", () => {
-    TodoController.createTodo();
-    expect(TodoModel.create).toBeCalled();
+    let req, res, next;
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+    req.body = newTodo;
+    TodoController.createTodo(req, res, next);
+    expect(TodoModel.create).toBeCalledWith(newTodo);
   });
 });
