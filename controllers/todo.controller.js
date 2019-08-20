fe0d5169932cbd4cd2fbbd74c5ed0627a3493286
stefan <stefan@stefans-iMac.local>
2019-08-20 02:16:15 +0200

    Verifying json body is returned

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,6 +1,6 @@
 const TodoModel = require("../model/todo.model");
 
 exports.createTodo = (req, res, next) => {
-  TodoModel.create(req.body);
-  res.status(201).send();
+  const createdModel = TodoModel.create(req.body);
+  res.status(201).json(createdModel);
 };
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -13,18 +13,25 @@ beforeEach(() => {
 });
 
 describe("TodoController.createTodo", () => {
+  beforeEach(() => {
+    req.body = newTodo;
+  });
+
   it("should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
   });
   it("should call TodoModel.create", () => {
-    req.body = newTodo;
     TodoController.createTodo(req, res, next);
     expect(TodoModel.create).toBeCalledWith(newTodo);
   });
   it("should return 201 response code", () => {
-    req.body = newTodo;
     TodoController.createTodo(req, res, next);
     expect(res.statusCode).toBe(201);
     expect(res._isEndCalled()).toBeTruthy();
+  });
+  it("should return json body in response", () => {
+    TodoModel.create.mockReturnValue(newTodo);
+    TodoController.createTodo(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(newTodo);
   });
 });
