b73436af1250535934b2acd2b87deb13974ff903
stefan <stefan@stefans-iMac.local>
2019-08-22 19:00:21 +0200

    Fixed failing tests

--- mongodb/mongodb.connect.js
+++ mongodb/mongodb.connect.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     await mongoose.connect(
-      "mongodb://SuperTestUser:SuperTestUser1@ds211648.mlab.com:11648/todo-tdd"
+      "mongodb://SuperTestUser:SuperTestUser1@ds211648.mlab.com:11648/todo-tdd",
+      { useNewUrlParser: true }
     );
   } catch (err) {
     console.error("Error connecting to mongodb");
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -24,14 +24,14 @@ describe("TodoController.createTodo", () => {
     TodoController.createTodo(req, res, next);
     expect(TodoModel.create).toBeCalledWith(newTodo);
   });
-  it("should return 201 response code", () => {
-    TodoController.createTodo(req, res, next);
+  it("should return 201 response code", async () => {
+    await TodoController.createTodo(req, res, next);
     expect(res.statusCode).toBe(201);
     expect(res._isEndCalled()).toBeTruthy();
   });
-  it("should return json body in response", () => {
+  it("should return json body in response", async () => {
     TodoModel.create.mockReturnValue(newTodo);
-    TodoController.createTodo(req, res, next);
+    await TodoController.createTodo(req, res, next);
     expect(res._getJSONData()).toStrictEqual(newTodo);
   });
 });
