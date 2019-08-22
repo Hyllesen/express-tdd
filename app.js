abb48ed05e888621283d9e913ce45e6196ac39d0
stefan <stefan@stefans-iMac.local>
2019-08-22 18:37:37 +0200

    Connected to mlab

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const todoRoutes = require("./routes/todo.routes");
 const app = express();
+const mongodb = require("./mongodb/mongodb.connect");
+
+mongodb.connect();
 
 app.use(express.json());
 
---
+++ mongodb/mongodb.connect.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+async function connect() {
+  try {
+    await mongoose.connect(
+      "mongodb://SuperTestUser:SuperTestUser1@ds211648.mlab.com:11648/todo-tdd"
+    );
+  } catch (err) {
+    console.error("Error connecting to mongodb");
+    console.error(err);
+  }
+}
+
+module.exports = { connect };
