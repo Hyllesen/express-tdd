5c7da0415dfe39d580005e3e6c8d32b77124f56b
stefan <stefan@stefans-iMac.local>
2019-08-28 11:11:00 +0200

    Unit tests done

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -10,6 +10,10 @@ exports.createTodo = async (req, res, next) => {
 };
 
 exports.getTodos = async (req, res, next) => {
-  const allTodos = await TodoModel.find({});
-  res.status(200).json(allTodos);
+  try {
+    const allTodos = await TodoModel.find({});
+    res.status(200).json(allTodos);
+  } catch (err) {
+    next(err);
+  }
 };
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -29,6 +29,13 @@ describe("TodoController.getTodos", () => {
     expect(res._isEndCalled()).toBeTruthy();
     expect(res._getJSONData()).toStrictEqual(allTodos);
   });
+  it("should handle errors in getTodos", async () => {
+    const errorMessage = { message: "Error finding" };
+    const rejectedPromise = Promise.reject(errorMessage);
+    TodoModel.find.mockReturnValue(rejectedPromise);
+    await TodoController.getTodos(req, res, next);
+    expect(next).toHaveBeenCalledWith(errorMessage);
+  });
 });
 
 describe("TodoController.createTodo", () => {
