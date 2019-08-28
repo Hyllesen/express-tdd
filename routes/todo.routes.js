3f795f4c21e3847238eb94d38acf1f66133a6a1b
stefan <stefan@stefans-iMac.local>
2019-08-28 11:38:36 +0200

    Integration test done

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -3,5 +3,6 @@ const todoController = require("../controllers/todo.controller");
 const router = express.Router();
 
 router.post("/", todoController.createTodo);
+router.get("/", todoController.getTodos);
 
 module.exports = router;
--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -5,6 +5,14 @@ const newTodo = require("../mock-data/new-todo.json");
 const endpointUrl = "/todos/";
 
 describe(endpointUrl, () => {
+  test("GET " + endpointUrl, async () => {
+    const response = await request(app).get(endpointUrl);
+
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBeTruthy();
+    expect(response.body[0].title).toBeDefined();
+    expect(response.body[0].done).toBeDefined();
+  });
   it("POST " + endpointUrl, async () => {
     const response = await request(app)
       .post(endpointUrl)
