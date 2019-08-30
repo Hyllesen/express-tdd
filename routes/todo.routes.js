9f3fc602940eb1a8930fce06a25e503e0407ec3a
stefan <stefan@stefans-iMac.local>
2019-08-30 11:10:17 +0200

    Put integration test

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.post("/", todoController.createTodo);
 router.get("/", todoController.getTodos);
 router.get("/:todoId", todoController.getTodoById);
+router.put("/:todoId", todoController.updateTodo);
 
 module.exports = router;
--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -4,7 +4,7 @@ const newTodo = require("../mock-data/new-todo.json");
 
 const endpointUrl = "/todos/";
 
-let firstTodo;
+let firstTodo, newTodoId;
 
 describe(endpointUrl, () => {
   test("GET " + endpointUrl, async () => {
@@ -35,6 +35,7 @@ describe(endpointUrl, () => {
     expect(response.statusCode).toBe(201);
     expect(response.body.title).toBe(newTodo.title);
     expect(response.body.done).toBe(newTodo.done);
+    newTodoId = response.body._id;
   });
   it(
     "should return error 500 on malformed data with POST" + endpointUrl,
@@ -48,4 +49,13 @@ describe(endpointUrl, () => {
       });
     }
   );
+  it("PUT " + endpointUrl, async () => {
+    const testData = { title: "Make integration test for PUT", done: true };
+    const res = await request(app)
+      .put(endpointUrl + newTodoId)
+      .send(testData);
+    expect(res.statusCode).toBe(200);
+    expect(res.body.title).toBe(testData.title);
+    expect(res.body.done).toBe(testData.done);
+  });
 });
